bdb40bd62115e90030bae13e17776e314514a0be
ramonrp99 <ramonrodped@gmail.com>
2026-02-19 00:15:48 +0100

    [style] Eliminar salto de línea en rateLimit.js

--- backend/src/middlewares/rateLimit.js
+++ backend/src/middlewares/rateLimit.js
@@ -3,8 +3,7 @@ const { rateLimit } = require('express-rate-limit')
 const aiLimiter = rateLimit({
     windowMs: (Number.parseInt(process.env.AI_LIMIT_WINDOW_MINS) || 60) * 1000,
     limit: Number.parseInt(process.env.AI_LIMIT_MAX) || 50,
-    message: 'Se ha alcanzado límite de peticiones por hora.',
-
+    message: 'Se ha alcanzado límite de peticiones por hora.'
 })
 
 module.exports = { aiLimiter }
