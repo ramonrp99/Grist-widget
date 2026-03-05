6725a1e782e92a9953311164972a2eae323f024b
ramonrp99 <ramonrodped@gmail.com>
2026-03-05 20:27:32 +0100

    [style] Añade estilo a la interfaz
    - Se añade estilo y colores a la interfaz general y los componentes.
    - Se añade la fuente "Lato".
    - Se añade icono de "enviar".

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -37,11 +37,11 @@ export default function App() {
     }
 
     return (
-        <div>
-            <section>
+        <div className='flex flex-col h-full'>
+            <section className='flex-1 overflow-y-auto pb-4 mask-b-from-[calc(100%-1rem)] mask-b-to-100%'>
                 <ChatList messages={messages}/>
             </section>
-            <section>
+            <section className='p-2 border-2 border-neutral-200 rounded-xl shadow-md'>
                 <MessageInput onSend={(message, context, model) => sendMessage(message, context, model)}/>
             </section>
         </div>
--- frontend/src/components/MessageInput.tsx
+++ frontend/src/components/MessageInput.tsx
@@ -24,38 +24,47 @@ export default function MessageInput({onSend}: Readonly<MessageInputProps>) {
     return (
         <form
             action={sendMessage}
-            className="p-4 flex gap-2"
+            className="flex flex-col gap-2"
         >
-            <input
-                type="text"
-                id="message"
-                name="message"
-                placeholder="Escribe..."
-                className="border px-4 outline-none focus:ring-2"
-            />
-            <select 
-                name="context"
-                id="context"
-                className="border"
-            >
-                <option value="row">Fila seleccionada</option>
-                <option value="table">Tabla completa</option>
-            </select>
-            <select 
-                name="model"
-                id="model"
-                className="border"
-            >
-                {
-                    models.map(model => <option value={model.model} key={model.model}>{model.name}</option>)
-                }
-            </select>
-            <button
-                type="submit"
-                className=""
-            >
-                Enviar
-            </button>
+            <div>
+                <input
+                    type="text"
+                    id="message"
+                    name="message"
+                    placeholder="Escribe..."
+                    autoComplete="off"
+                    className="p-1 w-full focus:outline-none"
+                />
+            </div>
+            <div className="flex flex-row justify-between gap-2">
+                <div className="flex">
+                    <select 
+                        name="context"
+                        id="context"
+                        className="field-sizing-content h-8 p-1 border-2 rounded-full cursor-pointer bg-secondary text-primary border-primary font-semibold hover:bg-message-hover focus:outline-none focus:bg-message-hover"
+                    >
+                        <option value="row">Fila seleccionada</option>
+                        <option value="table">Tabla completa</option>
+                    </select>
+                </div>
+                <div className="flex min-w-0 gap-2">
+                    <select 
+                        name="model"
+                        id="model"
+                        className="field-sizing-content flex-1 min-w-0 h-8 p-1 border-2 rounded-full cursor-pointer bg-secondary text-primary border-primary font-semibold hover:bg-message-hover focus:outline-none focus:bg-message-hover"
+                    >
+                        {
+                            models.map(model => <option value={model.model} key={model.model}>{model.name}</option>)
+                        }
+                    </select>
+                    <button
+                        type="submit"
+                        className="flex h-8 p-1 rounded-full cursor-pointer bg-primary text-secondary hover:bg-primary-hover focus:outline-none focus-visible:ring-2 focus-visible:ring-black focus-visible:bg-primary-hover"
+                    >
+                        <span className="material-symbols-outlined">send</span>
+                    </button>
+                </div>
+            </div>
         </form>
     )
 }
